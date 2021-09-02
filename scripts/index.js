6f2bc5fe1317add9d779bfafb8748173e2c5d8d6
VerbindingMaken <74724974+VerbindingMaken@users.noreply.github.com>
2021-09-02 13:17:27 +0200

    Added function for random choice computer. And added function for showing player and computer choice with picture and word. Taken icons from iconFinder.com to make choice visible.

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,38 @@ const inpChoice = document.querySelector("#inp-choice");
 const btnInpChoice = document.querySelector("#btn-choice-submit");
 const inputContainer = document.querySelector("#input-container");
 
+/* Player choice */
+const playerChoicePicture = document.querySelector("#player-choice-picture");
+const playerChoiceWord = document.querySelector("#player-choice-word");
+
+/* Computer choice */
+const computerChoicePicture = document.querySelector("#computer-choice-picture");
+const computerChoiceWord = document.querySelector("#computer-choice-word");
+
 /* FEEDBACK */
 const feedbackCard = document.querySelector("#feedback-card");
 const explainResult = document.querySelector("#txt-explain-result");
 const winResult = document.querySelector("#txt-win-result");
 const btnPlayGame = document.querySelector("#btn-play-game");
+
+var options = [{
+    name: "",
+    src: "images/3017955_examination_inquiry_interrogation_investigation_poll_icon.svg",
+    alt: "Question Mark",
+    href: "https://www.iconfinder.com/iconsets/pinpoint-notifocation" }, {
+    name: "rock",
+    src: "images/2730260_harry_philosophers_potter_rock_solid_icon.svg",
+    alt: "Rock",
+    href: "https://www.iconfinder.com/search?q=rock&price=free" }, {
+    name: "paper",
+    src: "images/6351935_document_extension_file_folder_page_icon.svg",
+    alt: "Paper",
+    href: "https://www.iconfinder.com/search?q=paper&price=free"} , {
+    name: "scissors",
+    src: "images/326596_content_cut_scissors_icon.svg",
+    alt: "Scissors",
+    href: "https://www.iconfinder.com/search?q=scissors&price=free"
+    } ];
 
 /* Start of game */
 function startGame() {
@@ -53,4 +80,25 @@ function removeError() {
     feedbackCard.style.display = "none";
     btnPlayGame.style.display = "block";
     inpChoice.removeEventListener('focus', removeError);
+}
+
+/* Make a random choice for the computer */
+
+function makeRndChoice() {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    console.log(computerChoice);
+}
+
+/* Show player choice with picture and word */
+function showPlayerChoice(choiceNumber) {
+    playerChoicePicture.src = options[choiceNumber].src;
+    playerChoicePicture.alt = options[choiceNumber].alt;
+    playerChoicePicture.href = options[choiceNumber].href;
+    playerChoiceWord.textContent = options[choiceNumber].name;
+}
+function showComputerChoice(choiceNumber) {
+    computerChoicePicture.src = options[choiceNumber].src;
+    computerChoicePicture.alt = options[choiceNumber].alt;
+    computerChoicePicture.href = options[choiceNumber].href;
+    computerChoiceWord.textContent = options[choiceNumber].name;
 }
